9cfd8232db497c10def3fdefbae7a07c530d3e67
Ashutosh Mishra <ashutosh@vcbay.co>
2023-07-06 21:33:55 +0530

    note app

--- src/components/ArrayOfObject.js
+++ src/components/ArrayOfObject.js
@@ -1,56 +1,91 @@
 import React, { useState } from "react";
 
 const ArrayOfObject = () => {
-  const [students, setstudents] = useState([
-    { name: "Harshal", batch: "ece", package: "8lpa" },
-    { name: "Adarsh", batch: "cse", package: "9lpa" },
-    { name: "Vikrant", batch: "mech", package: "5lpa" },
-    { name: "Tarang", batch: "civil", package: "15lpa" },
-  ]);
-  const deleteMan = () => {
+  const [students, setstudents] = useState([]);
+  const deleteman = (index) => {
+    // index =2
     let oldStudents = [...students];
-    let newStudents = oldStudents.filter((v, i) => v.name !== "Adarsh");
+    let newStudents = oldStudents.filter((v, i) => i !== index);
     setstudents(newStudents);
   };
-  const deleteMan2 = () => {
-    let oldStudents = [...students];
-    let newStudents = oldStudents.filter((v, i) => v.name !== "Tarang");
-    setstudents(newStudents);
+  const updateMan = (currentIndex, newName) => {
+    const oldStudents = [...students];
+    oldStudents[currentIndex].name = newName;
+    setstudents(oldStudents);
   };
-  const deleteCurrent = (selectedStudent) => {
-    let oldStudents = [...students];
-    let newStudents = oldStudents.filter(
-      (v, i) => v.name !== selectedStudent.name
-    );
-    setstudents(newStudents);
+  const updateBranch = (currentIndex, newBranch) => {
+    const oldStudents = [...students];
+    oldStudents[currentIndex].batch = newBranch;
+    setstudents(oldStudents);
   };
+
+  const [studentName, setstudentName] = useState("");
+  const [packagee, setpackagee] = useState("");
+  const [batch, setbatch] = useState("");
+  const addstudent = () => {
+    const oldStudents = [...students];
+    let newStudent = {
+      name: studentName,
+      batch: batch,
+      package: packagee,
+    };
+    oldStudents.push(newStudent);
+    setstudents(oldStudents);
+  };
+
   return (
     <>
-      <h1>This is Students' database</h1>
+      <h1>This is my notes</h1>
+      <label>Write note title</label>
+      <input
+        value={studentName}
+        onChange={(e) => setstudentName(e.target.value)}
+        type="text"
+      />
+      <br />
+      <label>Write note description</label>
+      <input
+        value={batch}
+        onChange={(e) => setbatch(e.target.value)}
+        type="text"
+      />
+      <br />
+
+      <label>Write remark</label>
+      <input
+        value={packagee}
+        onChange={(e) => setpackagee(e.target.value)}
+        type="text"
+      />
+      <br />
+
+      <button onClick={() => addstudent()} type="button">
+        Add Notes
+      </button>
       {students.map((v, i) => {
         return (
           <div key={i}>
             <ul>
-              <li>Name: {v.name} </li>
-              <li> Batch: {v.batch} </li>
-              <li> Package: {v.package} </li>
-              <button onClick={() => deleteCurrent(v)} type="button">
+              <li>Note title: {v.name} </li>
+              <li> note description: {v.batch} </li>
+              <li> remarks: {v.package} </li>
+              <button onClick={() => deleteman(i)} type="button">
                 Delete Me
               </button>
+              <button onClick={() => updateMan(i, "Ashutosh")} type="button">
+                Update Me
+              </button>
+              {/* <button onClick={() => updateBranch(i, "CSE")} type="button">
+                Update Branch
+              </button> */}
             </ul>
           </div>
         );
       })}
-      <button onClick={() => deleteMan()} type="button">
-        {" "}
-        Delete Adarsh
-      </button>
-      <button onClick={() => deleteMan2()} type="button">
-        {" "}
-        Delete Tarang
-      </button>
     </>
   );
 };
 
 export default ArrayOfObject;
+
+// CRUD operation
